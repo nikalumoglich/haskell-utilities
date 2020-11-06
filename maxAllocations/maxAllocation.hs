634815b863194ae68f34e1deb5c4d04f4bdd192c
andre.mesquita <andre.mesquita@ewally.com.br>
2020-11-06 19:12:56 -0300

    Trocando foldr1 por foldr na soma dos subconjuntos, não dando erro em listas vazias

--- maxAllocations/maxAllocation.hs
+++ maxAllocations/maxAllocation.hs
@@ -1,10 +1,8 @@
-import Data.List
-
 stringToList :: String -> [Int]
 stringToList l = map (read) (words l)
 
 filterNext :: (Ord t, Num t) => [t] -> t -> [t] -> [[t]]
-filterNext stack capacity [] = []
+filterNext _ _ [] = []
 filterNext stack capacity (y:ys) = [ filter (\x -> x <= capacity - y) (stack ++ ys) ] ++ (filterNext (stack ++ [y]) capacity ys)
 
 validCombinations' :: (Num t, Ord t) => t -> [[t]] -> [t] -> [[t]]
@@ -17,5 +15,5 @@ validCombinations :: (Num t, Ord t) => t -> [t] -> [[t]]
 validCombinations capacity items = validCombinations' capacity (filterNext [] capacity items) items
 
 maxAllocation :: Int -> [Int] -> [Int]
-maxAllocation capacity sizes = foldr1 (\x y -> if foldr1 (+) x >= foldr1 (+) y then x else y) possiblePermutations where 
+maxAllocation capacity sizes = foldr1 (\x y -> if foldr (+) 0 x >= foldr (+) 0 y then x else y) possiblePermutations where 
   possiblePermutations = validCombinations capacity sizes
